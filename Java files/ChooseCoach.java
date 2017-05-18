1f8bcd2272ee7e1f56d96cf49fdb520a55d2cb6b
Jens Bäckvall <jensbackvall@t-nova.org>
2017-05-18 09:17:22 +0200

    update files

---
+++ Java files/ChooseCoach.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class ChooseCoach {
+
+    try {
+        //indhent data fra en fil med aktive trænere
+        Scanner input = new Scanner(new File("coach_test_list.txt"));
+        //definér en ArrayList at gemme de enkelte rækker i
+        ArrayList<String> coaches = new ArrayList<String>();
+        //gennemgå fil og opdel den i enkelte linjer som gemmes i ArrayList
+        while(input.hasNextLine()) {
+            coaches.add(input.nextLine());
+        }
+
+        //opdel ArrayList coaches i enkelte ArrayLists for hver træner, split ved mellemrum for at få celleindhold
+
+        //vælg træner
+    }}
+    catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    //test indhold af ArrayList coaches, for at se at den læser det ordentligt
+    System.out.println(coaches);
+
+}
---
+++ Java files/TestDolphin.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class TestDolphin {
+    public static void main(String[] args) {
+        ChooseCoach();
+    }
+}
